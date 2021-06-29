e736ffdf358a4bc3d6be643eee4ce019df1104f3
fang yuan <fanngyuan@gmail.com>
2021-06-29 17:03:23 +0800

    fix bug

--- src/main/java/org/starcoin/scan/constant/Constant.java
+++ src/main/java/org/starcoin/scan/constant/Constant.java
@@ -5,4 +5,5 @@ public final class Constant {
     public static final String TRANSACTION_INDEX = "txn_infos";
     public static final String TRANSACTION_EVENT_INDEX= "txn_events";
     public static final String UNCLE_BLOCK_INDEX = "uncle_blocks";
+    public static final String PendingTxnIndex = "pending_txns";
 }
