72ac375a98fb1d2bbbe1bf70042e7cd6c2b2ee0d
chengsuoyuan <suoyuan@gmail.com>
2021-08-09 16:27:47 +0800

    [token]fix unit test

--- src/test/java/org/starcoin/scan/bean/BlockTest.java
+++ src/test/java/org/starcoin/scan/bean/BlockTest.java
@@ -24,6 +24,6 @@ class BlockTest {
         header.setBlockHash("0xaa4a4f35742cd8b5f4b5bc2bd81f0f44def676e251a64e3bdb2ce8b78352230d");
         header.setAuthor("0x928b3032a3d78914b4e920627b93c62a");
         block.setHeader(header);
-        assertEquals("{\"header\":{\"author\":\"0x928b3032a3d78914b4e920627b93c62a\",\"block_hash\":\"0xaa4a4f35742cd8b5f4b5bc2bd81f0f44def676e251a64e3bdb2ce8b78352230d\",\"chain_id\":0,\"difficulty_number\":0,\"gas_used\":0,\"nonce\":0,\"number\":0}}", JSON.toJSONString(block));
+        assertEquals("{\"header\":{\"author\":\"0x928b3032a3d78914b4e920627b93c62a\",\"block_hash\":\"0xaa4a4f35742cd8b5f4b5bc2bd81f0f44def676e251a64e3bdb2ce8b78352230d\",\"chain_id\":0,\"difficulty_number\":0,\"gas_used\":0,\"nonce\":0,\"number\":0,\"timestamp\":0}}", JSON.toJSONString(block));
     }
 }
